485d66e54a15cdbea26f5c0c20445e2e3df6c3b9
KimDaJin <kimdajin514@gmail.com>
2024-06-12 16:42:44 +0900

    feat: define state

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,10 +1,28 @@
 part of 'number_trivia_bloc.dart';
 
 sealed class NumberTriviaState extends Equatable {
+  @override
+  List<Object?> get props => [];
+
   const NumberTriviaState();
 }
 
-final class NumberTriviaInitial extends NumberTriviaState {
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+  const Loaded({required this.trivia});
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [trivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+  const Error({required this.message});
+
+  @override
+  List<Object?> get props => [message];
 }
